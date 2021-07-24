668f119bffcdbb18cc3feab5a455437150a6c5ab
molodec3 <chernov.andrei@phystech.edu>
2021-07-25 02:15:28 +0300

    added model and loss

---
+++ loss.py
@@ -0,0 +1,111 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from utils import make_heatmap
+
+
+EPS = 1e-4
+
+
+def focal_loss(pred, truth, alpha, beta):
+    """
+    :param pred: model prediction heatmap batch_size x classes x H x W
+    :param truth: ground truth heatmap batch_size x classes x H x W
+    :param alpha: hyper parameter alpha
+    :param beta: hyper parameter beta
+    :return: focal loss
+    """
+
+    pos_labels = truth == 1
+    neg_labels = truth != 1
+
+    loss = torch.pow(1 - pred, alpha) * torch.log(pred) * pos_labels
+    loss += torch.pow(1 - truth, beta) * torch.pow(pred, alpha) * torch.log(1 - pred) * neg_labels
+
+    loss = -loss.sum()
+    if pos_labels.sum() != 0:
+        loss /= pos_labels.sum()
+
+    return loss
+
+
+def offset_loss(pred, truth, mask):
+    """
+    :param pred: model prediction offset batch_size x 2 x H x W
+    :param truth: ground truth offset batch_size x 2 x H x W
+    :param mask: truth mask
+    :return: offset_loss
+    """
+
+    pos_labels = mask == 1
+    loss = torch.abs(pred[pos_labels] - truth[pos_labels]).sum() / (pos_labels.sum() + EPS)
+
+    return loss
+
+
+def size_loss(pred, truth, mask):
+    """
+    :param pred: model prediction size batch_size x 2 x H x W
+    :param truth: ground truth size batch_size x 2 x H x W
+    :param mask: truth mask
+    :return: size_loss
+    """
+    pos_labels = mask == 1
+    loss = torch.abs(pred[pos_labels] - truth[pos_labels]).sum() / (pos_labels.sum() + EPS)
+
+    return loss
+
+
+def center_net_loss(
+        pred_heatmap, pred_offset, pred_size,
+        mask, size_tensor,
+        alpha=2, beta=4,
+        lambd_offset=1, lambd_size=0.1,
+        downsampling_ratio=4
+):
+    """
+    :param pred_heatmap:
+    :param pred_offset:
+    :param pred_size:
+    :param mask:
+    :param size_tensor:
+    :param alpha:
+    :param beta:
+    :param lambd_offset:
+    :param lambd_size:
+    :param downsampling_ratio:
+    :return:
+    """
+
+    idxs = (mask == 1).nonzero(as_tuple=True)
+    # get downsampled indexes
+    idxs_strided = (
+        idxs[0], idxs[1],
+        (idxs[2] / downsampling_ratio).floor().long(),
+        (idxs[3] / downsampling_ratio).floor().long()
+    )
+    mask_strided = torch.zeros([
+        mask.shape[0], mask.shape[1],
+        mask.shape[2] // downsampling_ratio,
+        mask.shape[3] // downsampling_ratio
+    ])
+    mask_strided[idxs_strided] = 1
+
+    focal = focal_loss(pred_heatmap, make_heatmap(mask_strided), alpha, beta)
+
+    real_offsets = torch.zeros([
+        mask.shape[0], mask.shape[1],
+        mask.shape[2] // downsampling_ratio,
+        mask.shape[3] // downsampling_ratio
+    ])
+    real_offsets[:, 0, idxs_strided[2], idxs_strided[3]] = \
+        idxs[2] / downsampling_ratio - idxs_strided[2]
+    real_offsets[:, 1, idxs_strided[2], idxs_strided[3]] = \
+        idxs[3] / downsampling_ratio - idxs_strided[3]
+
+    offset = offset_loss(pred_offset, real_offsets, mask_strided)
+
+    size = size_loss(pred_size, size_tensor, mask_strided)
+
+    return focal + lambd_offset * offset + lambd_size * size
---
+++ model.py
@@ -0,0 +1,56 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from torchvision import models
+
+
+class CenterNet(nn.Module):
+    def __init__(self, n_classes, resnet=18, pretrained=False):
+        super(CenterNet, self).__init__()
+
+        self.n_classes = n_classes
+
+        if resnet == 18:
+            backbone = models.resnet18(pretrained=pretrained)
+        elif resnet == 34:
+            backbone = models.resnet34(pretrained=pretrained)
+        elif resnet == 50:
+            backbone = models.resnet50(pretrained=pretrained)
+        elif resnet == 101:
+            backbone = models.resnet101(pretrained=pretrained)
+        elif resnet == 152:
+            backbone = models.resnet152(pretrained=pretrained)
+        else:
+            raise AttributeError('wrong resnet backbone')
+
+        self.backbone_l1 = nn.Sequential(*list(backbone.children())[:5])
+        self.backbone_l2 = list(backbone.children())[5]
+        self.backbone_l3 = list(backbone.children())[6]
+        self.backbone_l4 = list(backbone.children())[7]
+
+        if resnet <= 34:
+            self.deconv_l1 = nn.ConvTranspose2d(512, 256, kernel_size=2, stride=2)
+            self.deconv_l2 = nn.ConvTranspose2d(512, 128, kernel_size=2, stride=2)
+            self.deconv_l3 = nn.ConvTranspose2d(256, 64, kernel_size=2, stride=2)
+            self.pred = nn.Conv2d(128, self.n_classes + 4, kernel_size=3, padding=1)
+        else:
+            self.deconv_l1 = nn.ConvTranspose2d(2048, 1024, kernel_size=2, stride=2)
+            self.deconv_l2 = nn.ConvTranspose2d(2048, 512, kernel_size=2, stride=2)
+            self.deconv_l3 = nn.ConvTranspose2d(1024, 256, kernel_size=2, stride=2)
+            self.pred = nn.Conv2d(512, self.n_classes + 4, kernel_size=3, padding=1)
+
+    def forward(self, inp):
+        inp1 = self.backbone_l1(inp)
+        inp2 = self.backbone_l2(inp1)
+        inp3 = self.backbone_l3(inp2)
+        inp4 = self.backbone_l4(inp3)
+
+        out = self.deconv_l1(inp4)
+        out = self.deconv_l2(torch.cat([inp3, out], dim=1))
+        out = self.deconv_l3(torch.cat([inp2, out], dim=1))
+        out = self.pred(torch.cat([inp1, out], dim=1))
+
+        return out[:, :self.n_classes], \
+            out[:, self.n_classes:-2], \
+            out[:, -2:]
