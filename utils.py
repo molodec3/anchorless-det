af5a6192c242ff3db10114ba055816dfa3091d66
molodec3 <chernov.andrei@phystech.edu>
2021-07-31 21:31:47 +0300

    minor utils fixes

--- utils.py
+++ utils.py
@@ -13,30 +13,29 @@ def make_heatmap(mask, size_tensor):
     idxs = (mask == 1).nonzero(as_tuple=True)
     res_mask = torch.zeros(mask.shape)
 
-    for i in range(mask.shape[1]):
-        for b, y, x in zip(idxs[0], idxs[2], idxs[3]):
-            r = find_radius(size_tensor[i, 0, y, x], size_tensor[i, 1, y, x]).item() + EPS
+    for b, c, y, x in zip(idxs[0], idxs[1], idxs[2], idxs[3]):
+        r = find_radius(size_tensor[b, 0, y, x], size_tensor[b, 1, y, x]).item() + EPS
 
-            x_min, x_max = torch.max(torch.tensor(0.), x - r).floor().long().item(), \
-                           torch.min(x + r, torch.tensor(mask.shape[3])).floor().long().item()
-            y_min, y_max = torch.max(torch.tensor(0.), y - r).floor().long().item(), \
-                           torch.min(y + r, torch.tensor(mask.shape[2])).floor().long().item()
+        x_min, x_max = torch.max(torch.tensor(0.), x - r).floor().long().item(), \
+                       torch.min(x + r, torch.tensor(mask.shape[3])).floor().long().item()
+        y_min, y_max = torch.max(torch.tensor(0.), y - r).floor().long().item(), \
+                       torch.min(y + r, torch.tensor(mask.shape[2])).floor().long().item()
 
-            if x_max == mask.shape[3]:
-                x_max -= 1
-            if y_max == mask.shape[2]:
-                y_max -= 1
+        if x_max == mask.shape[3]:
+            x_max -= 1
+        if y_max == mask.shape[2]:
+            y_max -= 1
 
-            x_mean, y_mean = (x_min + x_max) // 2, (y_min + y_max) // 2
+        x_mean, y_mean = (x_min + x_max) // 2, (y_min + y_max) // 2
 
-            y_grid, x_grid = np.ogrid[
-                             y_min-y_mean:y_max-y_mean+1, x_min-x_mean:x_max-x_mean+1
-                             ]
-            c_mask = np.exp(-(x_grid**2 + y_grid**2) / (2 * r**2))
-            res_mask[b, i, y_min:y_max+1, x_min:x_max+1] = np.maximum(
-                c_mask, res_mask[b, i, y_min:y_max+1, x_min:x_max+1]
-            )
-            res_mask[b, i, y, x] = 1
+        y_grid, x_grid = np.ogrid[
+                         y_min-y_mean:y_max-y_mean+1, x_min-x_mean:x_max-x_mean+1
+                         ]
+        c_mask = np.exp(-(x_grid**2 + y_grid**2) / (2 * r**2))
+        res_mask[b, c, y_min:y_max+1, x_min:x_max+1] = np.maximum(
+            c_mask, res_mask[b, c, y_min:y_max+1, x_min:x_max+1]
+        )
+        # res_mask[b, c, y, x] = 1
     return res_mask
 
 
